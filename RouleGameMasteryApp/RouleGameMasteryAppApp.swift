394421d21d768db61fde8f787e45232957c487ab
ArtemMoiseev <moiseev@physics.msu.ru>
2025-03-22 19:43:14 +0300

    Initial Commit

---
+++ RouleGameMasteryApp/RouleGameMasteryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouleGameMasteryAppApp.swift
+//  RouleGameMasteryApp
+//
+//  Created by Artem on 22.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct RouleGameMasteryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
